5261cfe1e508f7da5fb0b3bf6e513e14beeac8f8
Elena <kunstvanelena@gmail.com>
2023-08-30 14:02:27 +0200

    add style, add event  handler functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-  return <div>app</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {};
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
